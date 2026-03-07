afd6f39e3b8a809c137479c79648781eee3c1774
AmitR2001 <amit.rathod@spit.ac.in>
2026-03-07 21:57:31 +0530

    Fix merge conflict in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import React from 'react';
-=======
-﻿import React from 'react';
->>>>>>> efa04fab56a99b2fd817ec62ef51439cb528ec9a
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
